6cc41a0bc3b7578454c0e450830fcb93fb159f67
nhr <33464885+nahorgebre@users.noreply.github.com>
2018-10-09 14:43:04 +0200

    -
    -

---
+++ RDS-deal-get.js
@@ -0,0 +1,25 @@
+// sengaaBackEnd/RDS-deals-get.fn
+const AWS = require('aws-sdk');
+var mysql = require('mysql');
+
+exports.fn = (event, context, callback) => {
+  var connection = mysql.createConnection({
+    host     : 'mysql-sengaa-aws.cugdacsemmim.us-east-1.rds.amazonaws.com',
+    user     : 'sengaa18',
+    password : 'aagnes18',
+    database : 'mysqlSengaaAws'
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected to database!');
+    var sql = "select * from mysqlSengaaAws.sengaa_deal where partner_id =" + event.partner_id;
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Query successfully executed!");
+      console.log(result);
+      context.succeed(result);
+      callback(null, result);
+    });
+  });
+}
